f078c8115a3af9f428c81c0294aedd2383a52b34
ninaadps <ninaadps@gmail.com>
2023-10-18 08:37:47 +0530

    lab

--- fibonacci.c
+++ fibonacci.c
@@ -1,32 +1,27 @@
-#include<omp.h>
 #include<stdio.h>
-long long fib[1000000];
-long long fibo(long long n)
-{
-    long long i=0,j=0;
-    if(n==1||n==0)
-    {
-        fib[n]=n;
-        return n;
-    }
-    if(n<0)return 0;
-    #pragma omp task shared(i)
-    i=fibo(n-1);
-    #pragma omp task shared(j)
-    j=fibo(n-2);
-    #pragma omp taskwait
-    fib[n]=i+j;
-    return fib[n];
-
+#include<stdlib.h>
+#include<omp.h>
+//fib
+int fib(int n){
+int i,j;
+if(n<2) return n;
+else{
+#pragma omp task shared(i) firstprivate(n)
+i = fib(n-1);
+#pragma omp task shared(j) firstprivate(n)
+j = fib(n-2);
+#pragma omp taskwait
+return i+j;
 }
-int main()
-{
-    long long n;
-    printf("Enter n");
-    scanf("%lld",&n);
-    #pragma omp parallel
-    fibo(n);
-    for(int i=0;i<n;i++)
-        printf("%lld\t \a",fib[i]);
-
+}
+void main(){
+int n = 20;
+double start = omp_get_wtime();
+#pragma omp parallel for
+for(int i=0;i<n;i++){
+int t = omp_get_thread_num();
+printf("thread: %d fib(%d) = %d\n",t,i,fib(i));
+}
+double end = omp_get_wtime();
+printf("using schedule time is : %f\n",end-start);
 }
